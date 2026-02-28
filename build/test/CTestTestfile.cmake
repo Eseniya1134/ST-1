1e84bbd2b2ef14ebfcc0133170de090a2b932a55
Eseniya1134 <shakirova.esya23@mail.ru>
2026-02-28 20:59:22 +0300

    laba is ready

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/e/VScode/ST-1/test
+# Build directory: /home/e/VScode/ST-1/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTests "/home/e/VScode/ST-1/build/test/ST-1.test")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/e/VScode/ST-1/test/CMakeLists.txt;29;add_test;/home/e/VScode/ST-1/test/CMakeLists.txt;0;")
+subdirs("../_deps/googletest-build")
--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,70 @@
 #include <cstdint>
 #include "alg.h"
 
-
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, ZeroAndOne) {
+    EXPECT_FALSE(checkPrime(0));
+    EXPECT_FALSE(checkPrime(1));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, SmallPrimes) {
+    EXPECT_TRUE(checkPrime(2));
+    EXPECT_TRUE(checkPrime(3));
+    EXPECT_TRUE(checkPrime(5));
+    EXPECT_TRUE(checkPrime(7));
+}
+
+TEST(CheckPrimeTest, SmallNonPrimes) {
+    EXPECT_FALSE(checkPrime(4));
+    EXPECT_FALSE(checkPrime(6));
+    EXPECT_FALSE(checkPrime(9));
+}
+
+TEST(CheckPrimeTest, LargerPrimes) {
+    EXPECT_TRUE(checkPrime(17));
+    EXPECT_TRUE(checkPrime(19));
+    EXPECT_TRUE(checkPrime(97));
+}
+
+TEST(CheckPrimeTest, LargerNonPrimes) {
+    EXPECT_FALSE(checkPrime(18));
+    EXPECT_FALSE(checkPrime(20));
+    EXPECT_FALSE(checkPrime(100));
+}
+
+TEST(NPrimeTest, FirstFewPrimes) {
+    EXPECT_EQ(nPrime(1), 2);
+    EXPECT_EQ(nPrime(2), 3);
+    EXPECT_EQ(nPrime(3), 5);
+    EXPECT_EQ(nPrime(4), 7);
+    EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(NPrimeTest, LaterPrimes) {
+    EXPECT_EQ(nPrime(10), 29);
+    EXPECT_EQ(nPrime(20), 71);
+    EXPECT_EQ(nPrime(50), 229);
+}
+
+TEST(NextPrimeTest, AfterSmallNumbers) {
+    EXPECT_EQ(nextPrime(4), 5);
+    EXPECT_EQ(nextPrime(11), 13);
+    EXPECT_EQ(nextPrime(17), 19);
+}
+
+TEST(NextPrimeTest, AfterLargerNumbers) {
+    EXPECT_EQ(nextPrime(100), 101);
+    EXPECT_EQ(nextPrime(200), 211);
+}
+
+TEST(SumPrimeTest, SmallRanges) {
+    EXPECT_EQ(sumPrime(0), 0);
+    EXPECT_EQ(sumPrime(2), 0);
+    EXPECT_EQ(sumPrime(10), 17);  
+    EXPECT_EQ(sumPrime(20), 77);  
+}
+
+TEST(SumPrimeTest, LargeRange) {
+    uint64_t res = sumPrime(2000000);
+    uint64_t expected = 142913828922;
+    EXPECT_EQ(res, expected);
 }
